31f985c938d7401e2850a9e65a97f5c93a0d37bf
Daniel Cañete <danielcanetecampos1978@gmail.com>
2026-07-19 08:29:38 +0200

    Add Firebase web configuration

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,12 @@
+import { FirebaseOptions } from '@angular/fire/app';
+
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: 'AIzaSyCvCgmDh5qO-pcpnqr2pdYqnYlxb7xQMpI',
+  authDomain: 'simple-estatico.firebaseapp.com',
+  databaseURL: 'https://simple-estatico.firebaseio.com',
+  projectId: 'simple-estatico',
+  storageBucket: 'simple-estatico.firebasestorage.app',
+  messagingSenderId: '776027025919',
+  appId: '1:776027025919:web:d2faeb157b9258aee777ca',
+  measurementId: 'G-64DLXYS1H8',
+};
