67b9d3ccafe03cd7bbf68213a673c3adab143940
Daniel Cañete <danielcanetecampos1978@gmail.com>
2026-07-19 08:29:58 +0200

    Initialize Firebase and Firestore

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 import { AppComponent } from './app/app.component';
+import { firebaseConfig } from './environments/firebase.config';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideIonicAngular()],
+  providers: [
+    provideIonicAngular(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
 }).catch((error: unknown) => console.error(error));
