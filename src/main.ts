7b25b0afec57d1259feabc66f64943ef8232c011
Daniel Cañete <danielcanetecampos1978@gmail.com>
2026-07-19 08:44:34 +0200

    fix(firebase): use Firebase SDK factories with AngularFire providers

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { provideAppInitializer, inject } from '@angular/core';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { inject, provideAppInitializer } from '@angular/core';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth } from '@angular/fire/auth';
+import { provideFirestore } from '@angular/fire/firestore';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import { AppComponent } from './app/app.component';
 import { AnonymousAuthService } from './app/core/firebase/anonymous-auth.service';
 import { firebaseConfig } from './environments/firebase.config';
